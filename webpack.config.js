c4d0abbc2e5647b1975b67ee0658904da1bdaef6
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-11-12 23:43:24 +0100

    Optimized build process: minified CSS, updated webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
 
 module.exports = {
   entry: "./src/app.js",
@@ -18,7 +22,26 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
     ],
   },
-  mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../css/styles.css",
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync([
+        path.join(__dirname, "src/**/*.js"),
+        path.join(__dirname, "public/**/*.html"),
+      ]),
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: ["...", new CssMinimizerPlugin()],
+  },
+  mode: "production",
 };
